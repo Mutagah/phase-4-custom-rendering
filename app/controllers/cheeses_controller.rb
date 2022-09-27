56ed224cc9826fb2cd801db306f17504a82d7a6b
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-27 12:34:22 +0300

    made use of custom error messages and only and execpt methods while rendering json data

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -7,9 +7,32 @@ class CheesesController < ApplicationController
   end
 
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #     id: cheese.id,
+  #     name: cheese.name,
+  #     price: cheese.price,
+  #     is_best_seller: cheese.is_best_seller
+  # }
+  # end
+
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, only:[:id, :name, :price, :is_best_seller]
+  # end
+
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, except: [:created_at, :updated_at]
+  # end
+
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
-
+    if cheese != nil
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else 
+      render json: {error: "Cheese not found"}, status: :not_found 
+    end
+end
 end
